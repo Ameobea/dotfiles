e92b745c06767ebb059c62101db6cef7d8b9d6b9
Casey Primozic <me@ameo.link>
2016-10-05 12:11:20 -0500

    Added some helper aliases for assembly programming and keyboard config

--- aliases.bash
+++ aliases.bash
@@ -15,8 +15,16 @@ alias cr="cargo run"
 alias cdoc="cargo rustdoc --open -- --no-defaults --passes collapse-docs --passes unindent-comments --passes strip-priv-imports"
 alias nightlies="curl -sSf https://static.rust-lang.org/rustup.sh | sh -s -- --channel=nightly"
 
+# ASM
+asmcompfunc() {
+  nasm -f elf -F dwarf -g $1.asm
+  ld -m elf_i386 -o $1 $1.o
+}
+alias asmcomp=asmcompfunc
+
 # Misc
 alias ogv2mp4="avconv -i out.ogv -c:v libx264 -preset veryslow -qp 0 -vf scale=\"1280:trunc(ow/a/2)*2\" -c:a libmp3lame -qscale:a 3 -ac 2 out.mp4"
+alias frkb="sudo dpkg-reconfigure keyboard-configuration"
 
 function mkcd {
   mkdir $1
