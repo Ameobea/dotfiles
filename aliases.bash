d97d0264b336a6c02717268d90c0d0716970f2f2
Casey Primozic <me@ameo.link>
2016-10-19 15:12:36 -0500

    Changed ls aliases to use exa  - Removed old ls aliases  - Removed one of Alex's old aliases

--- aliases.bash
+++ aliases.bash
@@ -4,8 +4,9 @@ alias rbp="source ~/.bash_profile"
 alias ral="source ~/.aliases.bash"
 alias bp="nano ~/.bash_profile"
 alias ..="cd .."
+alias l="exa"
 alias la="l -a"
-alias ll="l"
+alias ll="exa -l"
 alias path="echo $PATH | tr : '\n'"
 alias 2dotfiles="cd ~/dotfiles"
 
@@ -44,18 +45,6 @@ function swap {
   mv "$2" "$1"
   mv $TMPFILE "$2"
 }
-
-alias code="cd ~/code"
-
-# fancy ls command
-# -l  long format
-# -F  / after dirs, * after exe, @ after symlink
-# -G  colorize
-# -g suppress owner
-# -o suppress group
-# -h humanize sizes
-# -q print nongraphic chars as question marks
-alias l="ls -lFGgohq"
 
 # Misspellings aliases
 alias cd..="cd .."
