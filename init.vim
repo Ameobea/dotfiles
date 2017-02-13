4e2d3688e7fedfe7c9afd214d059c23035fb17d6
Casey Primozic <me@ameo.link>
2017-02-13 17:52:06 -0600

    Added nvim settings file and autolink it

---
+++ init.vim
@@ -0,0 +1,11 @@
+syntax on
+colorscheme lyla
+
+" tab settings
+filetype plugin indent on
+set tabstop=2
+set shiftwidth=2
+set expandtab
+
+" turn on line numbers
+set number
--- setup.bash
+++ setup.bash
@@ -1,5 +1,6 @@
 ln -s $PWD/bash_profile ~/.bash_profile
 ln -s $PWD/aliases.bash ~/.aliases.bash
+ln -s $PWD/init.vim ~/.config/nvim/
 
 # git submodule update --init --recursive
 # git submodule foreach git pull origin master
